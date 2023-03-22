f0c3753ce02d91314d5821fe26e2dbddb89dbc65
cameron <hammerbro_97@hotmail.com>
2023-03-22 16:07:09 -0600

    string length 20 return true

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function do_math(number_1, number_2){
+
+return (number_1 + number_2) * 5;
+
+
+}
+
+do_math(50, 50);
+
+function string_length(return_length_10){
+
+if(return_length_10 >= 10){
+
+    return true;
+}
+
+}
+
+string_length(`I_am_over_ten_length`.length);
