1b912d21027c0f20cd9e8f051a149f499b03e2b1
cameron <hammerbro_97@hotmail.com>
2023-03-24 15:33:48 -0600

    added variables and comments

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
+
+// This adds number_1 and number_2 then multiplies it by 5//
+
 function do_math(number_1, number_2) {
     return (number_1 + number_2) * 5;
 }
-do_math(50, 50);
+let math = do_math(50, 100);
+let math_2 = do_math(100, 200);
+let math_3 = do_math(175, 750);
+
+//it then cycles through these variables to achieve different results//
+
 console.log(`returned 500`);
-
-
-function do_math_2(number_3, number_4) {
-    return (number_3 + number_4) * 15;
-}
-do_math_2(250, 250);
 console.log(`returned 7500`);
-
-
-function do_math_3(number_5, number_6) {
-    return (number_5 + number_6) * 8;
-}
-do_math_3(550, 525);
 console.log(`returned 8600`);
 
+//logging the results from the debugger//
+
+//this function will return false or true depending on the length of the string it gets fed//
 
 function string_length(return_length_1) {
     if (return_length_1 >= 10) {
@@ -25,29 +24,20 @@ function string_length(return_length_1) {
     }
     return false;
 }
-string_length(`I_small`.length);
+
+
+//this is achieved by adding .length at the end of the string placed in the variable//
+
+let result_1 = string_length(`I_small`.length);
 console.log(`I_small: returned false`);
 
-
-function string_length_2(return_length_2) {
-    if (return_length_2 >= 10) {
-        return true;
-    }
-    return false;
-}
-string_length_2(`I_am_short`.length);
+let result_2 = string_length_2(`I_am_short`.length);
 console.log(`I_am_short: returned true`);
 
-
-function string_length_3(return_length_3) {
-    if (return_length_3 >= 10) {
-        return true;
-    }
-    return false;
-}
-string_length_3(`I_am_over_ten_length`.length);
+let result_3 = string_length_3(`I_am_over_ten_length`.length);
 console.log('I_am_over_ten_length: returned true');
 
+//results from debugger are logged//
 
 
 
